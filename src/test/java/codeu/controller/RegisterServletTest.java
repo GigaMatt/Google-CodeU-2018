707623daee7cb66b6c610ffc59a845f7b19c98ea
Azee <aziztitu1996@gmail.com>
2018-03-26 10:41:00 -0500

    Adds required tests in RegisterServletTest

--- src/test/java/codeu/controller/RegisterServletTest.java
+++ src/test/java/codeu/controller/RegisterServletTest.java
@@ -1,14 +1,19 @@
 package codeu.controller;
 
+import static org.junit.Assume.assumeNoException;
+
 import java.io.IOException;
 import java.time.Instant;
 import java.util.UUID;
 import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -43,5 +48,41 @@ public class RegisterServletTest {
     registerServlet.doGet(mockRequest, mockResponse);
 
     Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
+
+  @Test
+  public void testDoPost_InvalidUsername() throws IOException, ServletException {
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("<script>alert(\'This isn't safe\')</script>");
+    
+    registerServlet.doPost(mockRequest, mockResponse);
+
+    Mockito.verify(mockRequest).setAttribute("error", "Please enter only letters, numbers, and spaces.");
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
+
+  @Test
+  public void testDoPost_ExistingUser() throws IOException, ServletException {
+    registerServlet.setUserStore(mockUserStore);
+    
+    Mockito.when(mockUserStore.isUserRegistered("test username")).thenReturn(true);
+    
+    registerServlet.doPost(mockRequest, mockResponse);
+
+    Mockito.verify(mockRequest).setAttribute("error", "That username is already taken.");
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
+  
+  @Test
+  public void testDoPost_NewUser() throws IOException, ServletException {
+    registerServlet.setUserStore(mockUserStore);
+    
+    registerServlet.doPost(mockRequest, mockResponse);
+
+    Assert.assertEquals(mockUserStore.isUserRegistered("test username"), true);
+
+    User newUser = mockUserStore.getUser("test username");
+    Assert.assertEquals(BCrypt.checkpw("test password", newUser.getPassword()), true);
+
+    Mockito.verify(mockResponse).sendRedirect("/login");
   }
 }
