195a751710e4e69a334353d9bf296be3a5d1a7d1
dettma1 <32919333+dettma1@users.noreply.github.com>
2018-03-26 11:49:48 -0500

    removed and put indents back to try to make it look uniform

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -47,8 +47,8 @@ public class RegisterServlet extends HttpServlet {
 
   public void doPost(HttpServletRequest request, HttpServletResponse response)
   	throws IOException, ServletException {
-
-  		String username = request.getParameter("username");
+      
+      String username = request.getParameter("username");
       String password = request.getParameter("password");
       String passwordHash = BCrypt.hashpw(password, BCrypt.gensalt());
 
