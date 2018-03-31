9ee47f1be72ac0bf3ef4e7b23725a9198d528b84
dettma1 <32919333+dettma1@users.noreply.github.com>
2018-03-31 18:20:24 -0500

    added initial profile Page

---
+++ src/main/webapp/WEB-INF/view/profile.jsp
@@ -0,0 +1,43 @@
+<%--
+  Copyright 2017 Google Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+--%>
+<!DOCTYPE html>
+<html>
+  <head>
+   <title>Profile Page</title>
+   <link rel="stylesheet" href="/css/main.css">
+   <style>
+     label {
+       display: inline-block;
+       width: 100px;
+     }
+   </style>
+  </head>
+  <body>
+
+  <%@ include file="/include/navbar.jsp" %>
+
+<!-- prints the name of the user logged in, or null if no user is logged in-->
+   <div id="container">
+    <% if(request.getSession().getAttribute("user") != null){ %>
+        <h1><a><%= request.getSession().getAttribute("user") %></a></h1>
+    <% } else{ %>
+        <h1>You are not logged in!</h1>
+    <% } %>
+     
+     <p>Our User is working!</p>
+   </div>
+  </body>
+</html>
---
+++ src/test/java/codeu/controller/ProfileServletTest.java
@@ -0,0 +1,68 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package codeu.controller;
+
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.UUID;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+public class ProfileServletTest {
+
+  private ProfileServlet profileServlet;
+  private HttpServletRequest mockRequest;
+  private HttpServletResponse mockResponse;
+  private RequestDispatcher mockRequestDispatcher;
+  private HttpSession mockSession;
+  private UserStore mockUserStore;
+
+  @Before
+  public void setup() {
+    profileServlet = new ProfileServlet();
+
+    mockSession = Mockito.mock(HttpSession.class);
+
+    mockRequest = Mockito.mock(HttpServletRequest.class);
+    Mockito.when(mockRequest.getParameter("username")).thenReturn("test username");
+    Mockito.when(mockRequest.getParameter("password")).thenReturn("test password");
+    Mockito.when(mockRequest.getSession()).thenReturn(mockSession);
+
+    mockResponse = Mockito.mock(HttpServletResponse.class);
+
+    mockUserStore = Mockito.mock(UserStore.class);
+
+    mockRequestDispatcher = Mockito.mock(RequestDispatcher.class);
+    Mockito.when(mockRequest.getRequestDispatcher("/WEB-INF/view/profile.jsp"))
+        .thenReturn(mockRequestDispatcher);
+  }
+
+  @Test
+  public void testDoGet() throws IOException, ServletException {
+    profileServlet.doGet(mockRequest, mockResponse);
+    Mockito.verify(mockRequestDispatcher).forward(mockRequest, mockResponse);
+  }
+
+}
