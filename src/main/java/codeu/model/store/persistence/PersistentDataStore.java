98153557333354a5afdc5d2fd2a87248fba69f41
Azee <aziztitu1996@gmail.com>
2018-04-03 12:08:06 -0500

    Removed empty line for uniformity.

--- src/main/java/codeu/model/store/persistence/PersistentDataStore.java
+++ src/main/java/codeu/model/store/persistence/PersistentDataStore.java
@@ -24,7 +24,6 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
